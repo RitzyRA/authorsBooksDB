740eab292bc5ba0f512f0d4b4652591b2bb4b418
Rita <rita.audejaitiene@gmail.com>
2021-10-14 23:54:39 +0300

    Read and add data (using DB)

---
+++ Migrations/20211014191915_v1.1.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AuthorsBooksDB.Migrations
+{
+    public partial class v11 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AuthorBook",
+                columns: table => new
+                {
+                    AuthorId = table.Column<int>(type: "int", nullable: false),
+                    BookId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorBook", x => new { x.AuthorId, x.BookId });
+                    table.ForeignKey(
+                        name: "FK_AuthorBook_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AuthorBook_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "Name", "Surname" },
+                values: new object[,]
+                {
+                    { 1, "Jonas", "Biliūnas" },
+                    { 2, "Balys", "Sruoga" },
+                    { 3, "Žemaitė", null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Marti" },
+                    { 2, "Dievų miškas" },
+                    { 3, "Kliudžiau" },
+                    { 4, "Liūdna pasaka" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AuthorBook",
+                columns: new[] { "AuthorId", "BookId" },
+                values: new object[,]
+                {
+                    { 3, 1 },
+                    { 2, 2 },
+                    { 1, 3 },
+                    { 1, 4 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorBook_BookId",
+                table: "AuthorBook",
+                column: "BookId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuthorBook");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AuthorsBooksDB
+{
+    class Program
+    {
+        private static AuthorsBooksDBAppDbContext _context = new AuthorsBooksDBAppDbContext();
+        static void Main()
+        {
+            //GetAuthors();
+            //GetBooks();
+            GetAllAuthorsWBooks();
+            AddAuthorWBook(new Author("Rūta", "Šepetys"), new Book("Tarp pilkų debesų"));
+            Console.WriteLine("---------------------------------------------------------");
+            GetAllAuthorsWBooks();
+        }
+        static void GetAuthors()
+        {
+            var al = _context.Authors.ToListAsync();
+            foreach (var author in al.Result)
+                Console.WriteLine(author);
+        }
+        static void GetBooks()
+        {
+            var bl = _context.Books.ToListAsync();
+            foreach (var book in bl.Result)
+                Console.WriteLine(book);
+        }
+        static void GetAllAuthorsWBooks()
+        {
+            var authors = _context.Authors.Include("Books");
+            foreach (var author in authors)
+            {
+                foreach (var book in author.Books)
+                {
+                    Console.WriteLine($"{ author} - {book}");
+                }
+            }
+        }
+        static void AddAuthorWBook(Author author, Book book)
+        {
+            Author firstAuthor = null;
+            try
+            {
+                firstAuthor = _context.Authors.Include("Books").Where(a => a.Name == author.Name && a.Surname == author.Surname).First();
+                
+            }
+            catch (InvalidOperationException e) when (e.Message.Contains("Sequence contains no elements"))
+            {
+                _context.Authors.Add(author);
+                _context.SaveChanges();
+                firstAuthor = _context.Authors.Include("Books").Where(a => a.Name == author.Name && a.Surname == author.Surname).First();
+            }
+            finally
+            {
+                firstAuthor.Books.Add(book);
+                _context.SaveChanges();
+            }
+        }
+    }
+    class AuthorsBooksDBAppDbContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source=.; database=AuthorsBooksDB; integrated security=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>().HasData(
+                new Author(1, "Jonas", "Biliūnas"),
+                new Author(2, "Balys", "Sruoga"),
+                new Author() { Id = 3, Name = "Žemaitė" }
+            );
+            modelBuilder.Entity<Book>().HasData(
+                new Book(1, "Marti"),
+                new Book(2, "Dievų miškas"),
+                new Book(3, "Kliudžiau"),
+                new Book(4, "Liūdna pasaka")
+            );
+            modelBuilder.Entity<Book>()
+                .HasMany(b => b.Authors)
+                .WithMany(a => a.Books)
+                .UsingEntity<AuthorBook>(
+                    ab => ab.HasOne<Author>().WithMany(),
+                    ab => ab.HasOne<Book>().WithMany()
+                );
+
+            modelBuilder.Entity<AuthorBook>().HasData(
+                new AuthorBook() { AuthorId = 1, BookId = 3 },
+                new AuthorBook() { AuthorId = 1, BookId = 4 },
+                new AuthorBook() { AuthorId = 2, BookId = 2 },
+                new AuthorBook() { AuthorId = 3, BookId = 1 }
+            );
+
+        }
+    }
+    class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public List<Book> Books { get; set; }
+        public Author(int id, string name, string surname)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+        }
+        public Author(string name)
+        {
+            Name = name;
+        }
+        public Author(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+        public Author() { }
+        public override string ToString()
+        {
+            return $"{{ Id: {Id} : Name: { Name } { Surname } }}";
+        }
+    }
+    class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<Author> Authors { get; set; }
+
+        public Book(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+        public Book(string title)
+        {
+            Title = title;
+        }
+        public override string ToString()
+        {
+            return $"{{ Id: {Id} : Title: { Title } }}";
+        }
+    }
+    class AuthorBook
+    {
+        public int AuthorId { get; set; }
+        public int BookId { get; set; }
+    }
+}
